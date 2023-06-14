72ece92fd5d5b2922b7fbaf3c8c7d679167fa4dd
luiluma <luma24lopez@gmail.com>
2023-06-13 21:15:30 -0500

    Print and count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    for number in range(x):
+        try:
+            print("{:d}".format(my_list[number]), end='')
+            length += 1
+        except (TypeError, ValueError):
+            pass
+    print('')
+    return length
